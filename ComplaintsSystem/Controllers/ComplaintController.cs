50e79e34a1d2aff09813afacaef9898e3b9a4fac
Muhammad Shahid Ali <shahid.ali@outlook.com>
2020-05-03 18:11:24 +0500

    Complaint Management System

---
+++ ComplaintsSystem/Controllers/ComplaintController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ComplaintsSystem.Models;
+using ComplaintsSystem.Services;
+using ComplaintsSystem.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComplaintsSystem.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComplaintController : ControllerBase
+    {
+
+        private IUserServices _userservice;
+        private IComplaintService _complaintService;
+
+        public ComplaintController(IUserServices userservice, IComplaintService complaintService)
+        {
+            _userservice = userservice;
+            _complaintService = complaintService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login([FromBody]LoginModel model)
+        {
+            var user = _userservice.Authenticate(model.Username, model.Password);
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+
+        // GET: api/Complaint
+        [HttpGet("get")]
+        public List<Complaint> Get()
+        {
+            return _complaintService.Get();
+        }
+
+        // POST: api/Complaint
+        [HttpPost("save")]
+        public IActionResult Save([FromBody]ComplaintModel model)
+        {
+            _complaintService.Save(model);
+            return Ok();
+        }
+
+    }
+}
---
+++ ComplaintsSystem/Services/UserServices.cs
@@ -0,0 +1,57 @@
+﻿using ComplaintsSystem.Models;
+using ComplaintsSystem.ViewModels;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplaintsSystem.Services
+{
+    public class UserServices : IUserServices
+    {           
+        private readonly CMSContext _db;
+        private readonly IConfiguration _configuration;
+
+        public UserServices(CMSContext db, IConfiguration configuration)
+        {           
+            _db = db;
+            _configuration = configuration;
+        }
+
+        public UserModel Authenticate(string email, string password)
+        {
+            var user = _db.Users.SingleOrDefault(x => x.UserEmail == email && x.Password == password && x.IsActive == true);
+
+            // return null if user not found
+            if (user == null)
+                return null;
+
+            // authentication successful so generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["SecretKey"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.UserEmail.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            UserModel usermdl = new UserModel() { Email = user.UserEmail, IsActive = (bool)user.IsActive, Token = tokenHandler.WriteToken(token) };
+
+
+            return usermdl;
+        }
+    }
+
+    
+}
